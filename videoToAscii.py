b1def904190171e73b48d65d5b9fd56c2fd78a28
Allen Sarkisyan <programminglinguist@gmail.com>
2015-05-30 19:57:56 -0700

    init commit

---
+++ videoToAscii.py
@@ -0,0 +1,44 @@
+import cv2
+
+def transform(img):
+	transformedAscii = []
+	for i in img:
+		temp = []
+		for j in i:
+			temp.append(asciiToNum[j])
+		transformedAscii.append(temp)
+	return transformedAscii
+
+def setupAsciiMapping():
+	asciiToNum = {}
+	characterSet = list((' '*2)+('n'*2)+('n'*1)+('/'*3)+('.'*2)+(' '*23))
+	for i in range(26):
+		for j in range(10):
+			asciiToNum[i*10+j]=characterSet[i]
+	return asciiToNum
+    
+def arrayToString(arr):
+	ascii = ''
+	for i in transformedAscii:
+		ascii+= ' '.join(i)
+		ascii+='\n'
+	return ascii
+            
+asciiToNum = setupAsciiMapping()
+
+width,height = 80,60
+
+cap = cv2.VideoCapture(0)
+ret = cap.set(3,width)
+ret = cap.set(4,height)
+
+
+while True:
+	ret,frame = cap.read()
+	img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	transformedAscii = transform(img)	
+	print arrayToString(transformedAscii)
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
